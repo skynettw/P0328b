1df1718456959a7e27a6f0a2dba855652829bd18
skynettw <skynet.tw@gmail.com>
2023-03-28 12:01:13 +0800

    加入專案檔案。

---
+++ P0328b/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P0328b
+{
+    class Car
+    {
+        private string color = "Red";
+        private string model = "Focus";
+        private int maxSpeed = 180;
+
+        public Car(string c)
+        {
+            color = c;
+        }
+        public Car(string c, string m)
+        {
+            color = c;
+            model = m;
+        }
+
+        public Car(string c, string m, int s)
+        {
+            color = c;
+            model = m;
+            maxSpeed = s;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"My Model:{model}");
+            Console.WriteLine($"My Color:{color}");
+            Console.WriteLine($"My MaxSpeed:{maxSpeed}");
+        }
+
+        public void ChangeColor(string newColor)
+        {
+            color = newColor;
+        }
+    }
+}
---
+++ P0328b/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P0328b
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("White", "Focus", 180);
+            Car Lexus = new Car("Red", "IS300", 250);
+            Ford.Info();
+            Lexus.Info();
+            Lexus.ChangeColor("Golden");
+            Lexus.Info();
+            Console.ReadLine();
+        }
+    }
+}
